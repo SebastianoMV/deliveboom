58494388a724ee160e4bae318303a4561964bbcc
SebastianoMV <sebastianovisco@gmail.com>
2022-08-31 12:57:27 +0200

    CardComp debug

--- public/js/front.js
+++ public/js/front.js
@@ -2028,7 +2028,130 @@ var staticRenderFns = [function () {
   var _vm = this,
       _c = _vm._self._c;
 
-  return _c("div", [_c("h4", [_vm._v("Carrello")])]);
+  return _c("div", [_c("div", {
+    staticClass: "container debug mt-5"
+  }, [_c("div", {
+    staticClass: "row"
+  }, [_c("div", {
+    staticClass: "col-6 debug p-4 text-center"
+  }, [_c("h4", [_vm._v("Carrello")])]), _vm._v(" "), _c("div", {
+    staticClass: "col-6 debug p-4 text-center"
+  }, [_c("h4", [_vm._v("Checkout")])])]), _vm._v(" "), _c("div", {
+    staticClass: "row"
+  }, [_c("div", {
+    staticClass: "col-8 debug"
+  }, [_c("div", {
+    staticClass: "food py-4 debug d-flex justify-content-between align-items-center"
+  }, [_c("img", {
+    attrs: {
+      src: __webpack_require__(/*! ../../../../public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg */ "./public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg"),
+      alt: ""
+    }
+  }), _vm._v(" "), _c("div", {
+    staticClass: "prodotto"
+  }, [_c("h5", [_vm._v("Pizza Pepperoni")]), _vm._v(" "), _c("div", {
+    staticClass: "descrizione"
+  }, [_vm._v("Size:Medium")]), _vm._v(" "), _c("div", {
+    staticClass: "prezzo"
+  }, [_vm._v("9")])]), _vm._v(" "), _c("div", {
+    staticClass: "quantity"
+  }, [_c("h6", [_vm._v("Quantity")]), _vm._v(" "), _c("input", {
+    attrs: {
+      type: "number"
+    }
+  }), _vm._v(" "), _c("div", {
+    staticClass: "remove"
+  }, [_vm._v("Remove")])])]), _vm._v(" "), _c("div", {
+    staticClass: "food py-4 debug d-flex justify-content-between align-items-center"
+  }, [_c("img", {
+    attrs: {
+      src: __webpack_require__(/*! ../../../../public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg */ "./public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg"),
+      alt: ""
+    }
+  }), _vm._v(" "), _c("div", {
+    staticClass: "prodotto"
+  }, [_c("h5", [_vm._v("Pizza Pepperoni")]), _vm._v(" "), _c("div", {
+    staticClass: "descrizione"
+  }, [_vm._v("Size:Medium")]), _vm._v(" "), _c("div", {
+    staticClass: "prezzo"
+  }, [_vm._v("9")])]), _vm._v(" "), _c("div", {
+    staticClass: "quantity"
+  }, [_c("h6", [_vm._v("Quantity")]), _vm._v(" "), _c("input", {
+    attrs: {
+      type: "number"
+    }
+  }), _vm._v(" "), _c("div", {
+    staticClass: "remove"
+  }, [_vm._v("Remove")])])]), _vm._v(" "), _c("div", {
+    staticClass: "food py-4 debug d-flex justify-content-between align-items-center"
+  }, [_c("img", {
+    attrs: {
+      src: __webpack_require__(/*! ../../../../public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg */ "./public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg"),
+      alt: ""
+    }
+  }), _vm._v(" "), _c("div", {
+    staticClass: "prodotto"
+  }, [_c("h5", [_vm._v("Pizza Pepperoni")]), _vm._v(" "), _c("div", {
+    staticClass: "descrizione"
+  }, [_vm._v("Size:Medium")]), _vm._v(" "), _c("div", {
+    staticClass: "prezzo"
+  }, [_vm._v("9")])]), _vm._v(" "), _c("div", {
+    staticClass: "quantity"
+  }, [_c("h6", [_vm._v("Quantity")]), _vm._v(" "), _c("input", {
+    attrs: {
+      type: "number"
+    }
+  }), _vm._v(" "), _c("div", {
+    staticClass: "remove"
+  }, [_vm._v("Remove")])])]), _vm._v(" "), _c("div", {
+    staticClass: "food py-4 debug d-flex justify-content-between align-items-center"
+  }, [_c("img", {
+    attrs: {
+      src: __webpack_require__(/*! ../../../../public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg */ "./public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg"),
+      alt: ""
+    }
+  }), _vm._v(" "), _c("div", {
+    staticClass: "prodotto"
+  }, [_c("h5", [_vm._v("Pizza Pepperoni")]), _vm._v(" "), _c("div", {
+    staticClass: "descrizione"
+  }, [_vm._v("Size:Medium")]), _vm._v(" "), _c("div", {
+    staticClass: "prezzo"
+  }, [_vm._v("9")])]), _vm._v(" "), _c("div", {
+    staticClass: "quantity"
+  }, [_c("h6", [_vm._v("Quantity")]), _vm._v(" "), _c("input", {
+    attrs: {
+      type: "number"
+    }
+  }), _vm._v(" "), _c("div", {
+    staticClass: "remove"
+  }, [_vm._v("Remove")])])])]), _vm._v(" "), _c("div", {
+    staticClass: "col-4 debug"
+  }, [_c("h5", [_vm._v("Total")]), _vm._v(" "), _c("h5", [_vm._v("$24")]), _vm._v(" "), _c("label", {
+    attrs: {
+      "for": "comments"
+    }
+  }, [_vm._v("Additional comments")]), _vm._v(" "), _c("textarea", {
+    attrs: {
+      name: "comments",
+      id: "comments",
+      cols: "30",
+      rows: "10"
+    }
+  }), _vm._v(" "), _c("label", {
+    attrs: {
+      "for": "comments"
+    }
+  }, [_vm._v("Apply promo code")]), _vm._v(" "), _c("input", {
+    staticClass: "form-control",
+    attrs: {
+      type: "text",
+      placeholder: "promo"
+    }
+  }), _vm._v(" "), _c("button", {
+    staticClass: "btn btn-danger w-100 my-3 d-block"
+  }, [_vm._v("Apply promo code")]), _vm._v(" "), _c("button", {
+    staticClass: "btn btn-danger w-100 my-3 d-block"
+  }, [_vm._v("Checkout")])])])])]);
 }];
 render._withStripped = true;
 
@@ -6634,6 +6757,25 @@ exports.i(__webpack_require__(/*! -!../../node_modules/css-loader!bootstrap/dist
 
 // module
 exports.push([module.i, "", ""]);
+
+// exports
+
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/pages/CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true&":
+/*!************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--8-2!./node_modules/sass-loader/dist/cjs.js??ref--8-3!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/pages/CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true& ***!
+  \************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".debug[data-v-04ef5265] {\n  border: 1px solid black;\n  background-color: rgba(255, 0, 0, 0.3);\n}\n.food img[data-v-04ef5265] {\n  width: 30%;\n}", ""]);
 
 // exports
 
@@ -37905,6 +38047,36 @@ if(false) {}
 
 /***/ }),
 
+/***/ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/pages/CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true&":
+/*!****************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/style-loader!./node_modules/css-loader!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--8-2!./node_modules/sass-loader/dist/cjs.js??ref--8-3!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/pages/CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true& ***!
+  \****************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+var content = __webpack_require__(/*! !../../../../node_modules/css-loader!../../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../../node_modules/postcss-loader/src??ref--8-2!../../../../node_modules/sass-loader/dist/cjs.js??ref--8-3!../../../../node_modules/vue-loader/lib??vue-loader-options!./CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true& */ "./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/pages/CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true&");
+
+if(typeof content === 'string') content = [[module.i, content, '']];
+
+var transform;
+var insertInto;
+
+
+
+var options = {"hmr":true}
+
+options.transform = transform
+options.insertInto = undefined;
+
+var update = __webpack_require__(/*! ../../../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
+
+if(content.locals) module.exports = content.locals;
+
+if(false) {}
+
+/***/ }),
+
 /***/ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/partials/FooterComp.vue?vue&type=style&index=0&id=de99f5e8&lang=scss&scoped=true&":
 /*!*********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/style-loader!./node_modules/css-loader!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--8-2!./node_modules/sass-loader/dist/cjs.js??ref--8-3!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/partials/FooterComp.vue?vue&type=style&index=0&id=de99f5e8&lang=scss&scoped=true& ***!
@@ -53111,6 +53283,17 @@ module.exports = function(module) {
 
 /***/ }),
 
+/***/ "./public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg":
+/*!*********************************************************************************************!*\
+  !*** ./public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg ***!
+  \*********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "../images/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg?136a78f7ee55c36c4bdd31b26e382ee2";
+
+/***/ }),
+
 /***/ "./resources/js/App.vue":
 /*!******************************!*\
   !*** ./resources/js/App.vue ***!
@@ -53254,7 +53437,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _CartComp_vue_vue_type_template_id_04ef5265_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./CartComp.vue?vue&type=template&id=04ef5265&scoped=true& */ "./resources/js/components/pages/CartComp.vue?vue&type=template&id=04ef5265&scoped=true&");
 /* harmony import */ var _CartComp_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./CartComp.vue?vue&type=script&lang=js& */ "./resources/js/components/pages/CartComp.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _CartComp_vue_vue_type_style_index_0_id_04ef5265_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true& */ "./resources/js/components/pages/CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
 
 
 
@@ -53262,7 +53447,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__["default"])(
   _CartComp_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _CartComp_vue_vue_type_template_id_04ef5265_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"],
   _CartComp_vue_vue_type_template_id_04ef5265_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
@@ -53291,6 +53476,22 @@ component.options.__file = "resources/js/components/pages/CartComp.vue"
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_CartComp_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib??ref--4-0!../../../../node_modules/vue-loader/lib??vue-loader-options!./CartComp.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/pages/CartComp.vue?vue&type=script&lang=js&");
 /* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_CartComp_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/pages/CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true&":
+/*!**************************************************************************************************************!*\
+  !*** ./resources/js/components/pages/CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true& ***!
+  \**************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_2_node_modules_sass_loader_dist_cjs_js_ref_8_3_node_modules_vue_loader_lib_index_js_vue_loader_options_CartComp_vue_vue_type_style_index_0_id_04ef5265_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/style-loader!../../../../node_modules/css-loader!../../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../../node_modules/postcss-loader/src??ref--8-2!../../../../node_modules/sass-loader/dist/cjs.js??ref--8-3!../../../../node_modules/vue-loader/lib??vue-loader-options!./CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true& */ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/pages/CartComp.vue?vue&type=style&index=0&id=04ef5265&lang=scss&scoped=true&");
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_2_node_modules_sass_loader_dist_cjs_js_ref_8_3_node_modules_vue_loader_lib_index_js_vue_loader_options_CartComp_vue_vue_type_style_index_0_id_04ef5265_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_2_node_modules_sass_loader_dist_cjs_js_ref_8_3_node_modules_vue_loader_lib_index_js_vue_loader_options_CartComp_vue_vue_type_style_index_0_id_04ef5265_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_2_node_modules_sass_loader_dist_cjs_js_ref_8_3_node_modules_vue_loader_lib_index_js_vue_loader_options_CartComp_vue_vue_type_style_index_0_id_04ef5265_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_2_node_modules_sass_loader_dist_cjs_js_ref_8_3_node_modules_vue_loader_lib_index_js_vue_loader_options_CartComp_vue_vue_type_style_index_0_id_04ef5265_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
 
 /***/ }),
 
@@ -53633,9 +53834,9 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Boolean\deliveboom\resources\js\front.js */"./resources/js/front.js");
-__webpack_require__(/*! C:\Boolean\deliveboom\resources\sass\front\style.scss */"./resources/sass/front/style.scss");
-module.exports = __webpack_require__(/*! C:\Boolean\deliveboom\resources\sass\admin\style.scss */"./resources/sass/admin/style.scss");
+__webpack_require__(/*! C:\LARAVEL\deliverboom\resources\js\front.js */"./resources/js/front.js");
+__webpack_require__(/*! C:\LARAVEL\deliverboom\resources\sass\front\style.scss */"./resources/sass/front/style.scss");
+module.exports = __webpack_require__(/*! C:\LARAVEL\deliverboom\resources\sass\admin\style.scss */"./resources/sass/admin/style.scss");
 
 
 /***/ })
--- resources/js/components/pages/CartComp.vue
+++ resources/js/components/pages/CartComp.vue
@@ -1,17 +1,96 @@
 <template>
     <div>
-
-        <h4>Carrello</h4>
-
+      <div class="container debug mt-5 ">
+        <div class="row">
+          <div class="col-6 debug p-4 text-center">
+            <h4>Carrello</h4>
+          </div>
+          <div class="col-6 debug p-4 text-center">
+            <h4>Checkout</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8 debug">
+            <div class="food py-4 debug d-flex justify-content-between align-items-center">
+                <img  src="../../../../public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg" alt="">
+                <div class="prodotto">
+                <h5>Pizza Pepperoni</h5>
+                <div class="descrizione">Size:Medium</div>
+                <div class="prezzo">9</div>
+              </div>
+              <div class="quantity">
+                <h6>Quantity</h6>
+                <input type="number">
+                <div class="remove">Remove</div>
+            </div>
+            </div>
+            <div class="food py-4 debug d-flex justify-content-between align-items-center">
+              <img  src="../../../../public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg" alt="">
+              <div class="prodotto">
+                <h5>Pizza Pepperoni</h5>
+                <div class="descrizione">Size:Medium</div>
+                <div class="prezzo">9</div>
+              </div>
+              <div class="quantity">
+                <h6>Quantity</h6>
+                <input type="number">
+                <div class="remove">Remove</div>
+            </div>
+            </div>
+            <div class="food py-4 debug d-flex justify-content-between align-items-center">
+              <img  src="../../../../public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg" alt="">
+              <div class="prodotto">
+                <h5>Pizza Pepperoni</h5>
+                <div class="descrizione">Size:Medium</div>
+                <div class="prezzo">9</div>
+              </div>
+              <div class="quantity">
+                <h6>Quantity</h6>
+                <input type="number">
+                <div class="remove">Remove</div>
+            </div>
+            </div>
+            <div class="food py-4 debug d-flex justify-content-between align-items-center">
+              <img  src="../../../../public/image/foods/202208310742immagini-magliette-diritto-autore-e1602668592213.jpg" alt="">
+              <div class="prodotto">
+                <h5>Pizza Pepperoni</h5>
+                <div class="descrizione">Size:Medium</div>
+                <div class="prezzo">9</div>
+              </div>
+              <div class="quantity">
+                <h6>Quantity</h6>
+                <input type="number">
+                <div class="remove">Remove</div>
+            </div>
+            </div>
+          </div>
+          <div class="col-4 debug">
+            <h5>Total</h5>
+            <h5>$24</h5>
+           <label for="comments">Additional comments</label>
+            <textarea name="comments" id="comments" cols="30" rows="10"></textarea>
+            <label for="comments">Apply promo code</label>
+            <input class="form-control" type="text" placeholder="promo">
+            <button class="btn btn-danger w-100 my-3 d-block">Apply promo code</button>
+            <button class="btn btn-danger w-100 my-3 d-block">Checkout</button>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-
-  <script>
-  export default {
-      name: 'CartComp',
-  }
-  </script>
-
-  <style lang="scss" scoped>
-
-  </style>
+    </template>
+      <script>
+      export default {
+          name: 'CartComp',
+      }
+      </script>
+      <style lang="scss" scoped>
+    .debug{
+            border: 1px solid black;
+            background-color: rgba(255, 0, 0, .3);
+        }
+    .food {
+      img{
+        width: 30%;
+      }
+    }
+      </style>
