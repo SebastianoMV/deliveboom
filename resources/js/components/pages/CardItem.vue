519b7066973408607cdfb745c8b127671ebd1395
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-08-31 18:12:17 +0200

    Card ristoranti

--- resources/js/components/pages/CardItem.vue
+++ resources/js/components/pages/CardItem.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="card-item">
-
-  </div>
+    <a href="#" class="card-item">
+        <div class="card-images">
+            <img src="../../../../public/image/typologies/tipology_07.jpg" alt="" class="thumbnail">
+            <img src="../../../../public/image/users/logo_03.png" alt="" class="restaurant-logo">
+        </div>
+        <h3>Sorbillo!</h3>
+        <p>Viva la pizza! La pizza è buona!</p>
+    </a>
 </template>
 
 <script>
@@ -11,5 +16,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.card-item{
+    width: 350px;
+    height: 360px;
+    color: black;
+    text-decoration: none;
+    margin-bottom: 30px;
+    box-shadow: 0px 0px 15px rgb(189, 189, 189);
+    border-radius: 10px;
+    overflow: hidden;
+    text-align: center;
+    .card-images{
+        position: relative;
+        width: 100%;
+        height: 62.5%;
+        margin-bottom: 40px;
+        .thumbnail{
+            height: 100%;
+            width: 100%;
+        }
+        .restaurant-logo{
+            height: 60px;
+            border-radius: 7px;
+            position: absolute;
+            bottom: 0;
+            right: 50%;
+            transform: translate(50%, 50%);
+        }
+    }
+    h3{
+        font-weight: bolder;
+    }
+    p{
+        font-size: 14px;
+        color: gray;
+    }
+}
 </style>
