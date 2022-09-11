c9a151753bbdbad4073f81c5f6af7f14278fba91
SebastianoMV <sebastianovisco@gmail.com>
2022-09-11 12:27:52 +0200

    modifica benvenuto

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card ">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Benvenuto {{ Auth::user()->name }}!
                 </div>
             </div>
         </div>
